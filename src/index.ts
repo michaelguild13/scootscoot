627da1269cee2956fa864651c3aff8023cb50511
Michael Guild <michaelguild13@gmail.com>
2019-04-20 13:02:37 -0400

    working on getting data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('init now')
+import * as mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
+import * as _ from 'lodash'
+const mapboxApiKey = 'pk.eyJ1IjoibWljaGFlbGd1aWxkMTMiLCJhIjoiY2p1cG0xYjl3MTZzeDQwcG52amt1c3dzeiJ9.nUFRPa2QZtIL4vTvGP5AmA'
+
+mapboxgl.accessToken = mapboxApiKey
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-77.009003, 38.889931],
+  zoom: 12
+})
+
+const birdApi = 'https://gbfs.bird.co/dc'
+
+const getData = async (url: string , filterPath: string) => {
+  const data = await fetch(url, { method: "GET", mode: "no-cors"})
+  console.log(data)
+  const filteredData = _.get(data, filterPath, [])
+  console.table(filteredData)
+}
+
+getData(birdApi, 'data.bikes')
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ var path = require('path')
 module.exports = {
   mode: 'development',
 
-  entry: './src/index.js',
+  entry: './src/index.ts',
 
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: ['ts-loader', 'standard-loader'],
         exclude: /node_modules/
       }
     ]
@@ -25,9 +25,16 @@ module.exports = {
   },
 
   devServer: {
+    headers: {
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': '*'
+    },
     contentBase: path.join(__dirname, 'public'),
     compress: true,
-    port: 9000,
+    host: '0.0.0.0',
+    port: 8000,
     hot: true
   }
 }
